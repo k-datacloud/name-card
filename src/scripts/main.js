cc91b604966274d349acd2230454086b3ddd6959
Kasiei <k25ts09@outlook.jp>
2026-01-07 18:07:29 +0900

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,39 +3,45 @@ import { gsap } from "gsap";
 const mainScript = () => {
   // modal
 
-  const modal = document.querySelector(".js-modal");
-  const modalWrapper = modal.querySelector(".modal__wrapper");
-  const modalMain = modal.querySelector(".modal__main");
-  const openModal = document.querySelector(".js-modal-open");
-  const closeModal = modal.querySelector(".js-modal-close");
+  const modalInit = () => {
+    const modal = document.querySelector(".js-modal");
+    if (!modal) return;
+    const modalWrapper = modal.querySelector(".modal__wrapper");
+    const modalMain = modal.querySelector(".modal__main");
+    const openModal = document.querySelector(".js-modal-open");
+    const closeModal = modal.querySelector(".js-modal-close");
 
-  gsap.set(modal, {
-    opacity: 0,
-    pointerEvents: "none",
-    zIndex: -1,
-  });
-
-  const open = () => {
-    gsap.to(modal, {
-      opacity: 1,
-      pointerEvents: "auto",
-      zIndex: 9999,
-    });
-  };
-
-  const close = () => {
-    gsap.to(modal, {
+    gsap.set(modal, {
       opacity: 0,
       pointerEvents: "none",
       zIndex: -1,
     });
+
+    const open = () => {
+      gsap.to(modal, {
+        opacity: 1,
+        pointerEvents: "auto",
+        zIndex: 9999,
+      });
+    };
+
+    const close = () => {
+      gsap.to(modal, {
+        opacity: 0,
+        pointerEvents: "none",
+        zIndex: -1,
+      });
+    };
+
+    openModal.addEventListener("click", open);
+    closeModal.addEventListener("click", close);
+    modalWrapper.addEventListener("click", close);
+    modalMain.addEventListener("click", (e) => {
+      e.stopPropagation();
+    });
   };
 
-  openModal.addEventListener("click", open);
-  closeModal.addEventListener("click", close);
-  modalWrapper.addEventListener("click", close);
-  modalMain.addEventListener("click", (e) => {
-    e.stopPropagation();
-  });
+  modalInit();
+  document.addEventListener("astro:page-load", modalInit);
 };
 export default mainScript;
